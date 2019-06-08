ad28cd17258760094ec1f03508d5029f94f20f5f
Rafal <Rafal.Wywrot@onet.pl>
2019-06-08 16:03:15 +0200

    Add project files.

---
+++ Library.Domain/LibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Library.Domain
+{
+    public class LibraryDbContext : DbContext
+    {
+        //dodaj connection stringa do bazy z azure
+        public LibraryDbContext() : base("DefaultConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
