b6e60e0413d9321c69b4bbff4399863af9d49bda
Rafal <Rafal.Wywrot@onet.pl>
2019-06-08 16:24:05 +0200

    add ninject

--- Library.Api/Controllers/HomeController.cs
+++ Library.Api/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.Domain.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,8 +10,16 @@ namespace Library.Api.Controllers
 {
     public class HomeController : ApiController
     {
+        private IBookService _bookService;
+        public HomeController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
         public IHttpActionResult Index()
         {
+            string a = _bookService.Get();
             return Ok();
         }
     }
---
+++ Library.Domain/Services/BookService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Services
+{
+    public interface IBookService
+    {
+        string Get();
+    }
+    public class BookService : IBookService
+    {
+        public string Get()
+        {
+            return "a2";
+        }
+    }
+}
