5d535d46b8245f517c47e4b7ee2695afe722825c
Rafal <Rafal.Wywrot@onet.pl>
2019-06-10 20:34:01 +0200

    add context

--- Library.Domain/LibraryDbContext.cs
+++ Library.Domain/LibraryDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using Library.Domain.Entities;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace Library.Domain
@@ -8,12 +9,16 @@ namespace Library.Domain
         //dodaj connection stringa do bazy z azure
         public LibraryDbContext() : base("DefaultConnection")
         {
-
+            
         }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<PublishHouse> PublishHouses { get; set; }
+        public DbSet<Artist> Artists { get; set; }
     }
 }
