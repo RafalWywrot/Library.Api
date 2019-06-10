e439a1676cd468afe93961f4ba475e5fee9b7182
Rafal <Rafal.Wywrot@onet.pl>
2019-06-10 23:58:58 +0200

    add artist service

--- Library.Domain/Services/ArtistService.cs
+++ Library.Domain/Services/ArtistService.cs
@@ -1,17 +1,18 @@
 ﻿using AutoMapper;
 using Library.Domain.DTO;
+using Library.Domain.Entities;
 using Library.Domain.Repositories;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library.Domain.Services
 {
     public interface IArtistService
     {
+        ArtistDTO Get(int id);
         IList<ArtistDTO> GetAll();
+        void Add(ArtistDTO artistDTO);
+        void Update(ArtistDTO artistDTO);
+        void Remove(int id);
     }
     public class ArtistService : IArtistService
     {
@@ -20,10 +21,33 @@ namespace Library.Domain.Services
         {
             this.artistRepository = artistRepository;
         }
-
+        public ArtistDTO Get(int id)
+        {
+            return Mapper.Map<ArtistDTO>(artistRepository.GetDetail(x => x.Id == id));
+        }
         public IList<ArtistDTO> GetAll()
         {
             return Mapper.Map<List<ArtistDTO>>(artistRepository.GetOverview());
+        }
+
+        public void Add(ArtistDTO artistDTO)
+        {
+            artistRepository.Add(Mapper.Map<Artist>(artistDTO));
+            artistRepository.SaveChanges();
+        }
+
+        public void Update(ArtistDTO artistDTO)
+        {
+            var artist = artistRepository.GetDetail(x => x.Id == artistDTO.Id);
+            Mapper.Map<ArtistDTO, Artist>(artistDTO, artist);
+            artistRepository.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var artist = artistRepository.GetDetail(x => x.Id == id);
+            artistRepository.Delete(artist);
+            artistRepository.SaveChanges();
         }
     }
 }
