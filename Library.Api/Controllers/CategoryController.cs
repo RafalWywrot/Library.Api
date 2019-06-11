3740e00e472e16262f4f0410312816d7483e90d1
Rafal <Rafal.Wywrot@onet.pl>
2019-06-11 19:34:54 +0200

    add categories api methods

---
+++ Library.Api/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Library.Api.Helpers;
+using Library.Domain.DTO;
+using Library.Domain.Services;
+using System.Web.Http;
+namespace Library.Api.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        private ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var categories = categoryService.GetAll();
+            return Ok(categories);
+        }
+        [HttpPost]
+        public IHttpActionResult New(CategoryDTO category)
+        {
+            categoryService.Add(category);
+            return Ok();
+        }
+        [HttpGet]
+        public IHttpActionResult GetById(int id)
+        {
+            var category = categoryService.Get(id);
+            return Ok(category);
+        }
+        [HttpPost]
+        public IHttpActionResult Update(CategoryDTO category)
+        {
+            categoryService.Update(category);
+            return Ok();
+        }
+        [HttpPost]
+        public IHttpActionResult Remove(EntityId entity)
+        {
+            categoryService.Remove(entity.Id);
+            return Ok();
+        }
+    }
+}
